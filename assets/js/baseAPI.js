1acc537e079caebaec99b4e8999e5a7366b2474f
babaofan <2285907229@qq.com>
2020-07-29 21:24:52 +0800

    完成了主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,5 +4,27 @@ $.ajaxPrefilter(function (options) {
     
     // 发起ajax请求前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
-    console.log(options.url)
+    // console.log(options.url)
+
+
+    // 统一为有权限的接口设置header请求头
+    if(options.url.indexOf('/my/') !==-1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+    // 全局统一挂在complete回调函数
+    options.complete = function (res) {
+        // console.log('执行了complete函数')
+        // console.log(res)
+        // 在complete回调函数中可以使用 res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 强制清空token
+            localStorage.removeItem('token')
+            // 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
